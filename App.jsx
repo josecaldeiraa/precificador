3928b15b85ec623a174884fd080e5670e9ffa899
josecaldeiraa <josecaldeiratst@gmail.com>
2025-07-31 13:31:13 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="p-6 text-center text-xl font-bold">
+      PrecificadorApp - Interface principal será implementada aqui.
+    </div>
+  )
+}
